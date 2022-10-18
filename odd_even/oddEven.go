511702ae834707ef211c529be42183edb0a95b40
Asjad Samdani <samdaniasjad143f@gmail.com>
2022-10-18 23:22:11 +0530

    checkoddEven

--- odd_even/oddEven.go
+++ odd_even/oddEven.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	a := 5
+	fmt.Println("Enter Numbers")
+	var n int
+	fmt.Scanln(&n)
+	if n%2 == 0 {
+		fmt.Println(n, "Number is even")
+	} else {
+		fmt.Println(n, "Number is odd")
 
-	fmt.Println(a)
-
+	}
 }
